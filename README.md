0c863a1eafe0e97f06304dfc6fa4f5ad7ff037f4
beibars96 <b.tangerkulov@gmail.com>
2024-06-27 21:39:03 -0500

    added labels and annotations

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 module "demo" {
   source = "beibars96/namespace/kubernetes"
   name   = "this-ns-demo"
+  labels = {
+    "env" = "demo"
+  }
+  annotations = {
+    "created-by" = "terraform"
+  }
 }
 
 ```
--- variable.tf
+++ variable.tf
@@ -4,3 +4,14 @@ variable "name" {
   default     = "this-ns"
 }
 
+variable labels {
+    type = map(string)
+    description = "a map of labels to add to the namespaces"
+    default = {} 
+}
+
+variable annotations {
+    type = map(string)
+    description = "a map of annotations to add to the namespaces"
+    default = {} 
+}
